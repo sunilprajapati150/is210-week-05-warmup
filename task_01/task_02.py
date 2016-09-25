c2c3770acfad688bf43da61ad87f3048dd3e47cd
Sunil Prajapati <nrk_619@yahoo.com>
2016-09-25 19:04:25 +0000

    Changes made

---
+++ task_01/task_02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Import a module namespace"""
+
+import task_01.py
+
+TIME = task_01.peanut.BUTTER
---
+++ task_01/task_03.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Copyign a module attribut"""
+
+from task_01.peanut import BUTTER
+
+JELLY = BUTTER
